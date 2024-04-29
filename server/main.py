eaf66cede6262e681f274a5f68e1f3a3feb8d08b
Anh Nguyen <anhngrx@gmail.com>
2024-04-29 15:53:59 +0700

    Remove unused files and update project structure

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ from utils.feature_extractor import extract_features
 from constants import BATCH_SIZE
 
 def main():
-  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser = argparse.ArgumentParser(description='Image Search System')
   parser.add_argument('--extract', action='store_true', help='Extract features')
   parser.add_argument('--serve', action='store_true', help='Start the web server')
   parser.add_argument('--evaluate', action='store_true', help='Evaluate the model')
